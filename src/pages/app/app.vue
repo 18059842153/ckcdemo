6b3f0b326998e8ef5f2d5450ae7dc0ad0b9a37c9
chenkecheng <chenkecheng@chenkechengdeMac-mini.local>
2017-08-07 14:14:55 +0800

    环境

---
+++ src/pages/app/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div >
+    111
+    </div>
+</template>
+<script>
+export default {
+    name: 'app',
+    data() {
+        return { 
+        }
+    },
+     components: {
+    },
+    methods: {
+        } 
+}
+</script>
